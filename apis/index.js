028e3cd0dc2c6cf0a3afb695aec78772d61e1bce
Mr. Redpolarbear <mrredpolarbear@gmail.com>
2017-12-14 14:49:06 -0800

    refactored -> api scrapers

---
+++ apis/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var scrapers = {};
+scrapers['mec'] = require('./scrapers/mec.scraper');
+
+exports.scrape = function (req, res, next) {
+  const url = req.body.url;
+  var scraperToUse = '';
+  
+  if (url.indexOf('mec.ca') !== -1) {
+    scraperToUse = 'mec'
+  } else {
+    scraperToUse = 'default'
+  }
+  scrapers[scraperToUse].list(url).then(function (cb_data) {
+    res.status(200).json(cb_data);
+  });
+}
