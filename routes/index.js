786960d0d4beb3f66b009215fd2c230239f9824f
Mr. Redpolarbear <mrredpolarbear@gmail.com>
2017-12-13 23:43:31 -0800

    mec

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const url = 'https://www.mec.ca/en/product/5024-150/Vioz-GT-GORE-TEX-Backpacking-Boots';
+  const url = 'https://www.mec.ca/en/product/5021-354/Power-Matic-200-GV-Backpacking-Boot';
   list(url).then(function(cb_data) {
-    res.send(cb_data);
+    res.status(200).json(cb_data);
   });
   // res.render('index', { title: 'Express' });
 });
@@ -36,21 +36,32 @@ function list (url) {
         this.emit('item', this.evaluate(function () {
           var $name = document.title;
           var $price = document.querySelector('#ProductDetailControls > div.product__controls__component.product__price.js-price > ul > li > span:nth-child(2) > span').textContent;
-          var $img = document.querySelector('#images > div.carousel.carousel--thumbnails.js-carousel-thumbnails > div > div > div > div.owl-item.synced > div > div > div > img');
+          var $imgElems = document.querySelectorAll('#images > div.carousel.carousel--thumbnails.js-carousel-thumbnails > div > div > div > div.owl-item');
+          var $imgsArray = [];
+          for (var i =0; i < $imgElems.length; ++i) {
+            var $img = $imgElems[i].querySelector('div > div > div > img')
+            $imgsArray.push($img.getAttribute('data-high-res-src'))
+          }
+          var $sizeOptions = document.querySelectorAll('#ProductDetailControls > div.product__controls__component.product__size > div > div.select-box > select > option');
+          var $sizeArray = [];
+          for (var i = 0; i < $sizeOptions.length; ++i) {
+            // var $size = $sizeOptions[i].getAttribute('value')
+            var $size = $sizeOptions[i].textContent.trim();
+            if ($size !== 'Select size' && $size.indexOf('unavailable') === -1) {
+              $sizeArray.push($size);
+            }
+          }
+          var $desc = document.querySelector('#pdp-description').innerHTML.trim();
           var item = {
             name: $name,
-            price: $price,
-            img: $img.getAttribute('data-high-res-src')
+            price: parseFloat($price.substr(1)),
+            imgs: $imgsArray,
+            size: $sizeArray,
+            desc: $desc
           };
           return item
         }));
       });
-      // spooky.then(function () {
-      //   this.emit('imgs', this.evaluate(function() {
-      //     var $img = document.querySelector('#images > div.carousel.carousel--thumbnails.js-carousel-thumbnails > div > div > div > div.owl-item.synced > div > div > div > img');
-      //     return $img.getAttribute('data-high-res-src')
-      //   }));
-      // });
       spooky.run();
     });
   
@@ -67,9 +78,9 @@ function list (url) {
         console.log(line);
     });
   
-    spooky.on('item', function (greeting) {
-      console.log(greeting);
-      resolve(greeting);
+    spooky.on('item', function (item) {
+      console.log(item);
+      resolve(item);
     });
     // spooky.on('imgs', function (greeting) {
     //   console.log(greeting);
